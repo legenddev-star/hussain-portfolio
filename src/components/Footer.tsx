908445f914cb7d83d67e52dc74c7d23f2169ad33
legenddev-star <thanzirhussain9@gmail.com>
2025-02-03 12:56:48 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,6 @@ import AIChatButton from "./AIChatButton";
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-white">
-      <AIChatButton />
     </footer>
   );
 }
